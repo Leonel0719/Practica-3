3f6acef62f92bca9295b9ec825f0fba871fdedd1
Leonel <126702603+Leonel0719@users.noreply.github.com>
2023-08-29 23:55:58 -0600

    Delete LADCH28082023.AppWeb directory

--- LADCH28082023.AppWeb/src/java/ladch28082023/appweb/utils/IAuthorize.java
+++
@@ -1,8 +0,0 @@
-package jgqz20230821.appweb.utils;
-
-import jakarta.servlet.ServletException;
-import java.io.IOException;
-
-public interface IAuthorize {
-    void authorize() throws ServletException, IOException;
-}
